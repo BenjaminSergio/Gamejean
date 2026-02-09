609de31b0ab1f67fb4ccb9bbe64a1aede11fcf3e
emanuelssergio <emanuelssergio@gmail.com>
2026-02-09 20:17:42 -0300

    loja

--- gamejean/cenas/passagem.gd
+++ gamejean/cenas/passagem.gd
@@ -1,17 +1,39 @@
 extends TextureButton
 @onready var painel = $"../../Control2"
 @onready var label: Label = $"../../Control2/TextureRect/Label"
+#nivel |preço
+#1     | 10
+#2     | 20
+#3     | 30
+#4     | 40
+#10 max | 100
+
+@onready var nivel: int =1
 func _ready():
+
 	painel.visible = false
 
 
 func _on_mouse_entered() -> void:
 	painel.visible = !painel.visible # Replace with function body.
 	label.text = "aumenta o preço pago da passagem dos alunos"
+	if nivel >= 10:
+		label.text = "BLOQUEADO"
 
 func _on_mouse_exited() -> void:
 	painel.visible = !painel.visible
 
 
 func _on_pressed() -> void:
-	pass
+	if nivel>=10:
+			print("jamanta")
+	if VariaveisGLobais.dinheiro_total>=(nivel*10):
+		print(nivel)
+		nivel+=1
+		VariaveisGLobais.dinheiro_total -= (nivel * 10)
+		VariaveisGLobais.preco_passagem=VariaveisGLobais.preco_passagem+(nivel * 0.25)
+		print(VariaveisGLobais.preco_passagem)
+		if nivel >= 11:
+			self.disabled = true
+			label.text = "BLOQUEADO"
+		
--- gamejean/cenas/variaveisGLobais.gd
+++ gamejean/cenas/variaveisGLobais.gd
@@ -1,13 +1,13 @@
 extends Node2D
 
 var dia_atual: int = 1
-var dinheiro_total: float =0.00
+var dinheiro_total: float =1150.00
 var satisfacao:float =5.0
 
 var preco_passagem:float =2.00
 var cota:float= 20.00
 var tempo_parada:float =15.0
-var aviso:int = 0
+var aviso:int = 2
 
 var persistencia_onibus: Dictionary = {}
 
